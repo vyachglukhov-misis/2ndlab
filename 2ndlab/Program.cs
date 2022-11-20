a788ef0c5e5a0b4a01c2e2bbf3662324e28894f1
vyachglukhov-misis <m2210359@edu.misis.ru>
2022-11-20 23:56:27 +0300

    second lab

---
+++ 2ndlab/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SecondLab
+{
+    struct Student {
+        public int Id;
+        public string FullName;
+        public DateTime BirthdayDate;
+        public string Institute;
+        public string Group;
+        public string Course;
+        public float AvgMark;
+
+        public Student(int id, string fullName, DateTime birthdayDate, string institute, string group, string course, float avgMark)
+        {
+            this.Id = id;
+            this.FullName = fullName;
+            this.BirthdayDate = birthdayDate;
+            this.Institute = institute;
+            this.Group = group;
+            this.Course = course;
+            this.AvgMark = avgMark;
+
+        }
+    }
+
+    class SecondLab {
+        public static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+
+}
---
+++ 2ndlab/methods.cs
@@ -0,0 +1,10 @@
+namespace methods {
+    public class Methods
+    {
+        public void FillStudentStruct()
+        {
+
+        }
+
+    }
+}
